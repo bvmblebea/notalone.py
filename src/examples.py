aac85c7c7deae180e5062e6bc5dc73c4437988ea
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-07-31 15:44:40 +0600

    Delete examples.py

--- src/examples.py
+++
@@ -1,5 +0,0 @@
-# simple login and getting user_id
-import notalone
-NEclient = notalone.NotAloneClient()
-NEClient.login(login="login", password="password")
-print(f"-- Account user_id is::: {client.user_id}")
